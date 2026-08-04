01ae8aa8f18fb960a991ac690626458286929f23
Fabien Cazenave <fabi1cazenave@users.noreply.github.com>
2026-08-04 16:16:57 +0200

    gniiiiiiiiii

--- include/aekeynox/emulations/hrm.h
+++ include/aekeynox/emulations/hrm.h
@@ -2,20 +2,20 @@
 // this is just a hack to re-define the base keymap,
 // keeping the same syntax whether HRMs are enabled or not.
 #if defined HT_NONE || defined HT_THUMB_TAPS
-  #undefine H_A
-  #undefine H_S
-  #undefine H_D
-  #undefine H_F
-  #undefine H_J
-  #undefine H_K
-  #undefine H_L
-  #undefine H_SEMI
+  #undef  H_A
   #define H_A &kp
+  #undef  H_S
   #define H_S &kp
+  #undef  H_D
   #define H_D &kp
+  #undef  H_F
   #define H_F &kp
+  #undef  H_J
   #define H_J &kp
+  #undef  H_K
   #define H_K &kp
+  #undef  H_L
   #define H_L &kp
+  #undef  H_SEMI
   #define H_SEMI &kp
 #endif
