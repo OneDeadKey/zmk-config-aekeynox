157e5b11827f7186e91796a5617aadaa0401989f
Fabien Cazenave <fabi1cazenave@users.noreply.github.com>
2026-08-25 10:46:34 +0200

    fix: shifted numbers

--- include/aekeynox/aliases.h
+++ include/aekeynox/aliases.h
@@ -55,6 +55,7 @@
 #elif defined KB_LAYOUT_QWERTZ_CH_DE || defined KB_LAYOUT_QWERTZ_CH_FR
   #include "aliases/qwertz_ch.h"
 #elifdef KB_LAYOUT_QWERTZ_CZ
+  #define SHIFTED_NUMBERS
   #include "aliases/qwertz_cz.h"
 #elifdef KB_LAYOUT_QWERTZ_DE
   #include "aliases/qwertz_de.h"
